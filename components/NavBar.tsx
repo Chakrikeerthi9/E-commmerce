6c2ab62a5c9bced110b212c5803b8c98b88022e5
Chakri Keerthi <keerthichakri2148@gmail.com>
2025-07-06 01:01:27 -0500

    In navbar carousel implemented by using stripe products catalogue

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className='sticky top-0 z-50 bg-white shadow'>
         <div className='container mx-auto flex items-center justify-between px-4 py-4'>
             <Link href="/" className='hover:text-blue-600'>
-                <h1>My E-Commerce Site</h1>
+                <h1>CK Store</h1>
             </Link>
         <div className='hidden md:flex space-x-6'>
             <Link href="/">Home</Link>
